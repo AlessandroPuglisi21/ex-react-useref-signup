a3e8a85f16c8ac32535855380af9f6c14da4e7f2
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-03-22 18:25:09 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex items-center justify-center bg-sky-200'>
+        <h1 className="p-4 text-3xl">FORM REGISTRAZIONE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

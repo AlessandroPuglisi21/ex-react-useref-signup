415882b4049a2ca67e13961dd6677d23e0595a95
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-03-22 19:23:16 +0100

    ADD:Milestone 1,prime validazioni

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,41 @@
+import { use } from 'react';
 import { useState } from 'react';
 
 function Form() {
     const submit = (e) => {
+
+        if (!name || !username || !password || !role || !exp) {
+            alert("Tutti i campi devono essere compilati!");
+            return;
+        }
+        if (username.length < 6) {
+            alert("Username deve essere di almeno 6 caratteri");
+            return;
+        }
+
+        if (exp < 0) {
+            alert("Anni di esperienza deve essere maggiore di 0");
+            return;
+        }
+
         e.preventDefault();
         console.log(`Submit eseguito con:
             Nome: ${name}
             Username: ${username}
             Password: ${password}
             Esperienza: ${exp}
-            Area: ${area}	`);
+            Area: ${area}	
+            ruolo: ${role}`);
 
     };
+
 
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [exp, setExp] = useState('');
+    const [exp, setExp] = useState(0);
     const [area, setArea] = useState('');
+    const [role, setRole] = useState({ value: '' });
 
     return (
         <div className="flex flex-col items-center mt-6">
@@ -31,7 +50,12 @@ function Form() {
                     placeholder="Nome e Cognome"
                 />
 
-
+                {/* USERNAME */}
+                <strong style={{ color: username.length < 6 ? 'red' : 'green' }}>
+                    {username.length < 6
+                        ? 'Username deve essere almeno di 6 caratteri'
+                        : 'Username valido'}
+                </strong>
                 <input
                     className="border border-black rounded-md p-2 w-full"
                     type="text"
@@ -39,6 +63,8 @@ function Form() {
                     onChange={(e) => setUsername(e.target.value)}
                     placeholder="Username"
                 />
+
+
 
 
                 <input
@@ -49,15 +75,30 @@ function Form() {
                     placeholder="Password"
                 />
 
-
-                <select className="border border-black p-2 rounded-md w-full" defaultValue="">
+                {/* RUOLO */}
+                <strong style={{ color: role.value === '' ? 'red' : 'green' }}>
+                    {role.value === ''
+                        ? 'Seleziona un ruolo'
+                        : 'Ruolo selezionato'}
+                </strong>
+                <select
+                    className="border border-black p-2 rounded-md w-full"
+                    value={role.value}
+                    onChange={(e) => setRole(e.target.value)}
+                >
                     <option value="">-</option>
                     <option value="fullstack">Full Stack</option>
                     <option value="frontend">Frontend</option>
                     <option value="backend">Backend</option>
                 </select>
 
+                {/* ESPERIENZA */}
+                <strong style={{ color: exp < 0 ? 'red' : 'green' }}>
+                    {exp < 0
+                        ? 'Anni di esperienza deve essere maggiore di 0'
+                        : 'Anni di esperienza valido'}
 
+                </strong>
                 <input
                     className="border border-black rounded-md p-2 w-full"
                     type="number"
